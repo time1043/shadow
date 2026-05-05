78015f688fa59d20b93d3c3b04c47cabc7fd97f2
oswin902m <1665434994@qq.com>
2026-05-05 23:10:38 +0800

    setup tanstack-router, vite-alias, oxfmt

---
+++ src/routes/__root.tsx
@@ -0,0 +1,20 @@
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+
+const RootLayout = () => (
+  <>
+    <div className="p-2 gap-2 flex">
+      <Link to="/" className="[&.active]:font-bold">
+        Home
+      </Link>{' '}
+      <Link to="/about" className="[&.active]:font-bold">
+        About
+      </Link>
+    </div>
+    <hr />
+    <Outlet />
+    <TanStackRouterDevtools />
+  </>
+);
+
+export const Route = createRootRoute({ component: RootLayout });
